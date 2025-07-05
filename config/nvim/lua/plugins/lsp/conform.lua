336297060cae302da0122d62abd439ffeeb0f2cc
yu-sz <yusuzuf00@gmail.com>
2025-07-05 15:37:43 +0900

    fix(neovim): integrate mason and conform

--- config/nvim/lua/plugins/lsp/conform.lua
+++ config/nvim/lua/plugins/lsp/conform.lua
@@ -1,35 +1,62 @@
 -- formatter settings
 return {
-  "stevearc/conform.nvim",
-  event = "VeryLazy",
-  opts = function()
-    local web_formatter = function()
-      return { "biome-check", "prettier", stop_after_first = true }
-    end
+	"stevearc/conform.nvim",
+	event = "VeryLazy",
+	opts = function()
+		local web_formatter_config = function()
+			return { "biome-check", "prettier", stop_after_first = true }
+		end
 
-    return {
-      web_formatter = {
-        lua = { "stylua" },
-        bash = { "shfmt" },
-        sql = { "sleek" },
-        -- Web
-        typescript = web_formatter,
-        javascript = web_formatter,
-        typescriptreact = web_formatter,
-        javascriptreact = web_formatter,
-        json = web_formatter,
-        jsonc = web_formatter,
-        yaml = { "prettier" },
-        html = web_formatter,
-        css = web_formatter,
-        scss = web_formatter,
-        less = web_formatter,
-      },
-      format_on_save = {
-        timeout_ms = 1500,
-        -- conformで定義したformatterが存在しないならLSPのフォーマッターを使う
-        lsp_format = "fallback",
-      },
-    }
-  end
+		return {
+			format_on_save = {
+				timeout_ms = 1500,
+				lsp_fallback = true,
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				bash = { "shfmt" },
+				typescript = web_formatter_config,
+				javascript = web_formatter_config,
+				typescriptreact = web_formatter_config,
+				javascriptreact = web_formatter_config,
+				json = web_formatter_config,
+				jsonc = web_formatter_config,
+				yaml = { "prettier" },
+				html = web_formatter_config,
+				css = web_formatter_config,
+				scss = web_formatter_config,
+				less = web_formatter_config,
+				markdown = web_formatter_config,
+			},
+			formatters = {
+				stylua = {
+					command = "stylua", -- 固定パスを削除
+				},
+				shfmt = {
+					command = "shfmt", -- 固定パスを削除
+				},
+				["biome-check"] = {
+					command = "biome", -- 固定パスを削除 (Masonは 'biome' と表示しているのでこれに合わせる)
+					args = { "format", "--stdin-filepath", "$FILENAME", "-" },
+					stdin = true,
+				},
+				prettier = {
+					command = "prettier", -- 固定パスを削除
+					args = { "--stdin-filepath", "$FILENAME" },
+					stdin = true,
+				},
+			},
+		}
+	end,
+	config = function(_, opts)
+		local conform = require("conform")
+		conform.setup(opts)
+		vim.keymap.set({ "n", "v" }, "<leader>lF", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
