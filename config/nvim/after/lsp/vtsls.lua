8fb8507ea88409aed9c6113ffa813f3e538053bb
yu-sz <yusuzuf00@gmail.com>
2025-09-20 23:57:42 +0900

    fix(neovim): vtsls setting

--- config/nvim/after/lsp/vtsls.lua
+++ config/nvim/after/lsp/vtsls.lua
@@ -1,55 +1,55 @@
 -- TypeScript lsp (wrapper for ts extension of vscode)
 ---@type vim.lsp.Config
 return {
-	cmd = { "vtsls", "--stdio" },
-	filetypes = {
-		"javascript",
-		"javascriptreact",
-		"javascript.jsx",
-		"typescript",
-		"typescriptreact",
-		"typescript.tsx",
-	},
-	settings = {
-		refactor_auto_rename = true,
-		complete_function_calls = true,
-		vtsls = {
-			enableMoveToFileCodeAction = true,
-			autoUseWorkspaceTsdk = true,
-			experimental = {
-				completion = {
-					enableServerSideFuzzyMatch = true,
-					entriesLimit = 20,
-				},
-			},
-		},
-		typescript = {
-			updateImportsOnFileMove = { enabled = "always" },
-			suggest = {
-				completeFunctionCalls = true,
-			},
-			inlayHints = {
-				parameterNames = { enabled = "literals" },
-				parameterTypes = { enabled = true },
-				variableTypes = { enabled = true },
-				propertyDeclarationTypes = { enabled = true },
-				functionLikeReturnTypes = { enabled = true },
-				enumMemberValues = { enabled = true },
-			},
-		},
-		javascript = {
-			updateImportsOnFileMove = { enabled = "always" },
-			suggest = {
-				completeFunctionCalls = true,
-			},
-			inlayHints = {
-				parameterNames = { enabled = "literals" },
-				parameterTypes = { enabled = true },
-				variableTypes = { enabled = true },
-				propertyDeclarationTypes = { enabled = true },
-				functionLikeReturnTypes = { enabled = true },
-				enumMemberValues = { enabled = true },
-			},
-		},
-	},
+  cmd = { "vtsls", "--stdio" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+  settings = {
+    refactor_auto_rename = true,
+    complete_function_calls = true,
+    vtsls = {
+      enableMoveToFileCodeAction = true,
+      autoUseWorkspaceTsdk = true,
+      experimental = {
+        completion = {
+          enableServerSideFuzzyMatch = true,
+          entriesLimit = 20,
+        },
+      },
+    },
+    typescript = {
+      updateImportsOnFileMove = { enabled = "always" },
+      suggest = {
+        completeFunctionCalls = true,
+      },
+      inlayHints = {
+        parameterNames = { enabled = "literals" },
+        parameterTypes = { enabled = true },
+        variableTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        functionLikeReturnTypes = { enabled = true },
+        enumMemberValues = { enabled = true },
+      },
+    },
+    javascript = {
+      updateImportsOnFileMove = { enabled = "always" },
+      suggest = {
+        completeFunctionCalls = true,
+      },
+      inlayHints = {
+        parameterNames = { enabled = "literals" },
+        parameterTypes = { enabled = true },
+        variableTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        functionLikeReturnTypes = { enabled = true },
+        enumMemberValues = { enabled = true },
+      },
+    },
+  },
 }
--- config/nvim/lua/plugins/lsp/conform.lua
+++ config/nvim/lua/plugins/lsp/conform.lua
@@ -1,62 +1,57 @@
 -- formatter settings
 return {
-	"stevearc/conform.nvim",
-	event = "VeryLazy",
-	opts = function()
-		local web_formatter_config = function()
-			return { "biome-check", "prettier", stop_after_first = true }
-		end
+  "stevearc/conform.nvim",
+  event = "VeryLazy",
+  opts = function()
+    local web_formatter_config = function()
+      return { "biome-check", "prettier", stop_after_first = true }
+    end
 
-		return {
-			format_on_save = {
-				timeout_ms = 1500,
-				lsp_fallback = true,
-			},
-			formatters_by_ft = {
-				lua = { "stylua" },
-				bash = { "shfmt" },
-				typescript = web_formatter_config,
-				javascript = web_formatter_config,
-				typescriptreact = web_formatter_config,
-				javascriptreact = web_formatter_config,
-				html = web_formatter_config,
-				css = web_formatter_config,
-				scss = web_formatter_config,
-				less = web_formatter_config,
-				json = web_formatter_config,
-				jsonc = web_formatter_config,
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-			},
-			formatters = {
-				stylua = {
-					command = "stylua", -- 固定パスを削除
-				},
-				shfmt = {
-					command = "shfmt", -- 固定パスを削除
-				},
-				["biome-check"] = {
-					command = "biome", -- 固定パスを削除 (Masonは 'biome' と表示しているのでこれに合わせる)
-					args = { "format", "--stdin-file-path", "$FILENAME", "-" },
-					stdin = true,
-				},
-				prettier = {
-					command = "prettier", -- 固定パスを削除
-					args = { "--stdin-filepath", "$FILENAME" },
-					stdin = true,
-				},
-			},
-		}
-	end,
-	config = function(_, opts)
-		local conform = require("conform")
-		conform.setup(opts)
-		vim.keymap.set({ "n", "v" }, "<leader>lF", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+    return {
+      format_on_save = {
+        timeout_ms = 1500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        bash = { "shfmt" },
+        typescript = web_formatter_config,
+        javascript = web_formatter_config,
+        typescriptreact = web_formatter_config,
+        javascriptreact = web_formatter_config,
+        html = web_formatter_config,
+        css = web_formatter_config,
+        scss = web_formatter_config,
+        less = web_formatter_config,
+        json = web_formatter_config,
+        jsonc = web_formatter_config,
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+      },
+      formatters = {
+        stylua = {
+          command = "stylua", -- 固定パスを削除
+        },
+        shfmt = {
+          command = "shfmt", -- 固定パスを削除
+        },
+        -- prettier = {
+        -- 	command = "prettier", -- 固定パスを削除
+        -- 	args = { "--stdin-filepath", "$FILENAME" },
+        -- 	stdin = true,
+        -- },
+      },
+    }
+  end,
+  config = function(_, opts)
+    local conform = require("conform")
+    conform.setup(opts)
+    vim.keymap.set({ "n", "v" }, "<leader>lF", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
 }
