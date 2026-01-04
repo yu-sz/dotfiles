c6090a07f96e2123a4ec30f63b0ef6d54a9db727
yu-sz <yusuzuf00@gmail.com>
2026-01-04 13:54:41 +0900

    fix(neovim): remove dependency to Telescope

--- config/nvim/lua/plugins/auto-session.lua
+++ config/nvim/lua/plugins/auto-session.lua
@@ -2,9 +2,6 @@
 return {
   "rmagatti/auto-session",
   lazy = false,
-  dependencies = {
-    "nvim-telescope/telescope.nvim", -- Only needed if you want to use session lens
-  },
   keys = {
     -- Will use Telescope if installed or a vim.ui.select picker otherwise
     { "<leader>wr", "<cmd>SessionRestore<CR>" },
