64280f781bfbe7d5bd12731e401b47f7839a52e8
yu-sz <yusuzuf00@gmail.com>
2026-01-11 01:33:25 +0900

    feat(neovim): create function for open pr diffview keymap

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -27,6 +27,29 @@ return {
       mode = "n",
       desc = "Toggle Git File History",
     },
+    {
+      "<leader>dp",
+      function()
+        local open_pull_request_diffview = function()
+          local handle = io.popen("gh pr view --json baseRefName --jq .baseRefName")
+          local base_branch = handle:read("*a"):gsub("%s+", "")
+          handle:close()
+
+          if base_branch ~= "" then
+            vim.cmd("DiffviewOpen " .. base_branch .. "...HEAD")
+            print("Opening Diffview against: " .. base_branch .. "...HEAD")
+          else
+            print("PR情報が見つかりません")
+          end
+        end
+
+        if next(require("diffview.lib").views) == nil then
+          open_pull_request_diffview()
+        else
+          vim.cmd("DiffviewClose")
+        end
+      end,
+    },
   },
   opts = {
     keymaps = {
