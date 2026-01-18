3c5d836f742a99fb1c04312ff32b43c3fd67d95e
yu-sz <yusuzuf00@gmail.com>
2026-01-18 20:22:13 +0900

    fix(neovim): hack!!truncate long TypeScript inlay hints

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -49,6 +49,57 @@ vim.api.nvim_create_autocmd("LspAttach", {
     -- reboot LSP
     keymap.set("n", "<leader><leader>r", "<cmd>LspRestart<CR>", opts)
 
+    -- HACK: デフォルトでlspから返ってくるtypescriptのinlay hintsが長々すぎるので、クライアント側で無理やり切り詰める
+    -- lsp設定などでサーバ側で切り詰めるなどは現状できないらしい
+    -- @See: https://www.reddit.com/r/neovim/comments/1c3iz5j/hack_truncate_long_typescript_inlay_hints/
+    local methods = vim.lsp.protocol.Methods
+    local inlay_hint_handler = vim.lsp.handlers[methods.textDocument_inlayHint]
+    vim.lsp.handlers[methods.textDocument_inlayHint] = function(err, result, ctx, config)
+      local client = vim.lsp.get_client_by_id(ctx.client_id)
+      if client and client.name == "vtsls" then
+        result = vim
+          .iter(result)
+          :map(function(hint)
+            local label = hint.label
+            if type(label) == "string" then
+              if label:len() >= 30 then
+                label = label:sub(1, 29) .. "…"
+              end
+              hint.label = label
+            elseif type(label) == "table" then
+              -- label is InlayHintLabelPart[]
+              local total_len = 0
+              for _, part in ipairs(label) do
+                total_len = total_len + part.value:len()
+              end
+              if total_len >= 30 then
+                local current_len = 0
+                hint.label = vim
+                  .iter(label)
+                  :map(function(part)
+                    if current_len >= 29 then
+                      return nil
+                    end
+                    local remaining = 29 - current_len
+                    if part.value:len() > remaining then
+                      part.value = part.value:sub(1, remaining) .. "…"
+                      current_len = 29
+                    else
+                      current_len = current_len + part.value:len()
+                    end
+                    return part
+                  end)
+                  :totable()
+              end
+            end
+            return hint
+          end)
+          :totable()
+      end
+
+      inlay_hint_handler(err, result, ctx, config)
+    end
+
     -- inlayhint settings
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
     if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
