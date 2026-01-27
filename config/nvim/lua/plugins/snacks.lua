a163a64abe62f4a8c14ed477885abbb41abac109
yu-sz <yusuzuf00@gmail.com>
2026-01-27 23:51:39 +0900

    feat(neovim): change snacks.explorer to floating layout

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -167,6 +167,23 @@ return {
         explorer = {
           hidden = true,
           ignored = true,
+          layout = {
+            { preview = true },
+            layout = {
+              box = "horizontal",
+              width = 0.8,
+              height = 0.8,
+              {
+                box = "vertical",
+                border = "rounded",
+                title = "{source} {live} {flags}",
+                title_pos = "center",
+                { win = "input", height = 1, border = "bottom" },
+                { win = "list", border = "none" },
+              },
+              { win = "preview", border = "rounded", width = 0.6, title = "{preview}" },
+            },
+          },
         },
         files = {
           hidden = true,
