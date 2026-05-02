3e3091e92a2226e517843e352de25d71702f6d37
yu-sz <yusuzuf00@gmail.com>
2026-05-02 14:10:44 +0900

    feat(sketchybar): redesign date with double bookend lines

--- config/sketchybar/items/date.lua
+++ config/sketchybar/items/date.lua
@@ -1,21 +1,40 @@
 local sbar = require("sketchybar")
 local colors = require("colors")
 
-local bracket_font = "Hiragino Sans:W3:28.0"
+local accent_outer = {
+  color = colors.blue,
+  border_color = colors.transparent,
+  border_width = 0,
+  height = 30,
+  corner_radius = 1,
+}
 
-sbar.add("item", "date.rbracket", {
+local accent_inner = {
+  color = colors.blue,
+  border_color = colors.transparent,
+  border_width = 0,
+  height = 30,
+  corner_radius = 0,
+}
+
+sbar.add("item", "date.accent.right_outer", {
   position = "right",
-  icon = {
-    string = "〕",
-    font = bracket_font,
-    color = colors.blue,
-    width = 14,
-    align = "left",
-    padding_left = 0,
-    padding_right = 0,
-  },
-  label = { drawing = false },
-  background = { drawing = false },
+  width = 2,
+  padding_left = 0,
+  padding_right = 0,
+  background = accent_outer,
+  icon = { drawing = false, padding_left = 0, padding_right = 0 },
+  label = { drawing = false, padding_left = 0, padding_right = 0 },
+})
+
+sbar.add("item", "date.accent.right_inner", {
+  position = "right",
+  width = 1,
+  padding_left = 0,
+  padding_right = 2,
+  background = accent_inner,
+  icon = { drawing = false, padding_left = 0, padding_right = 0 },
+  label = { drawing = false, padding_left = 0, padding_right = 0 },
 })
 
 local date = sbar.add("item", "date", {
@@ -25,35 +44,48 @@ local date = sbar.add("item", "date", {
     font = "Moralerspace Xenon HW:Bold:14.0",
     color = colors.fg,
     y_offset = 7,
-    padding_left = 4,
+    padding_left = 6,
     padding_right = 4,
   },
   label = {
     string = os.date("%b %d %a"),
     font = "Moralerspace Xenon HW:Regular:12.0",
-    color = colors.fg_dark,
+    color = colors.blue,
     y_offset = -7,
     padding_left = -40,
-    padding_right = 4,
+    padding_right = 6,
   },
   background = { drawing = false },
   update_freq = 30,
   updates = true,
 })
 
-sbar.add("item", "date.lbracket", {
+sbar.add("item", "date.accent.left_inner", {
   position = "right",
-  icon = {
-    string = "〔",
-    font = bracket_font,
-    color = colors.blue,
-    width = 14,
-    align = "left",
-    padding_left = -12,
-    padding_right = 0,
-  },
-  label = { drawing = false },
+  width = 1,
+  padding_left = 0,
+  padding_right = 0,
+  background = accent_inner,
+  icon = { drawing = false, padding_left = 0, padding_right = 0 },
+  label = { drawing = false, padding_left = 0, padding_right = 0 },
+})
+
+sbar.add("item", "date.accent.left_outer", {
+  position = "right",
+  width = 2,
+  padding_left = 0,
+  padding_right = 2,
+  background = accent_outer,
+  icon = { drawing = false, padding_left = 0, padding_right = 0 },
+  label = { drawing = false, padding_left = 0, padding_right = 0 },
+})
+
+sbar.add("item", "date.gap", {
+  position = "right",
+  width = 8,
   background = { drawing = false },
+  icon = { drawing = false, padding_left = 0, padding_right = 0 },
+  label = { drawing = false, padding_left = 0, padding_right = 0 },
 })
 
 date:subscribe("routine", function()
