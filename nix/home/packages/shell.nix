d19bf7a14d6ac806144fd07bc61014a29b1e3a56
yu-sz <yusuzuf00@gmail.com>
2026-05-04 21:29:23 +0900

    feat(shell): add miscellaneous cli tools

--- nix/home/packages/shell.nix
+++ nix/home/packages/shell.nix
@@ -2,14 +2,19 @@
 {
   home.packages = with pkgs; [
     btop
+    cmatrix
+    cowsay
     fd
     gitmux
     gomi
     hyperfine
     jq
+    lolcat
+    oha
     ripgrep
     sheldon
     smug
+    termdown
     wezterm
     zabrze
     zsh
