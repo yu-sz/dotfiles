a8883e8e65da1d804649b110b3f49a353e46d6c2
yu-sz <yusuzuf00@gmail.com>
2026-05-04 23:44:48 +0900

    feat(aerospace): allow free workspace-to-monitor moves

---
+++ config/aerospace/layout.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+aerospace workspace 3 && aerospace move-workspace-to-monitor secondary
+aerospace workspace 1 && aerospace move-workspace-to-monitor secondary
+aerospace workspace 2 && aerospace move-workspace-to-monitor main
+aerospace workspace 2
