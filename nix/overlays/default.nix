a1a4b707b2ab85ab8bafc0fc5b0a14ce1d4368f5
yu-sz <yusuzuf00@gmail.com>
2026-07-05 17:45:49 +0900

    chore(nix): drop obsolete direnv checkPhase workaround

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,7 +1,4 @@
-final: prev: {
+_final: prev: {
   zabrze = prev.callPackage ./zabrze.nix { };
   sketchybar-app-font = prev.callPackage ./sketchybar-app-font.nix { };
-
-  # direnv の workaround (詳細: ./direnv.nix)。
-  inherit (import ./direnv.nix { inherit (prev) lib; } final prev) direnv;
 }
