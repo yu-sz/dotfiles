f117dd5395e0a6525c3a7e0fd69e9da6eee7030d
yu-sz <yusuzuf00@gmail.com>
2026-07-09 21:47:45 +0900

    fix(zsh): harden interactive helper commands
    fkill defaulted to SIGKILL; workspace swallowed unknown
    subcommands into the picker and treated repo names as regex.

--- config/zsh/lazy/fzf.zsh
+++ config/zsh/lazy/fzf.zsh
@@ -8,9 +8,9 @@ export FZF_CTRL_T_COMMAND='rg --files --hidden --follow --glob "!**/.git/*"'
 export FZF_CTRL_T_OPTS="--preview 'bat --color=always --style=header,grid {}' --preview-window=right:60%"
 
 fkill() {
-    local pid
-    pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-    if [[ -n "$pid" ]]; then
-        echo "$pid" | xargs kill -${1:-9}
-    fi
+	local pid
+	pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+	if [[ -n "$pid" ]]; then
+		echo "$pid" | xargs kill -"${1:-15}"
+	fi
 }
