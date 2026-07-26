567faba19b362a1c9001279ba7eedef731b08823
yu-sz <yusuzuf00@gmail.com>
2026-07-26 15:58:44 +0900

    refactor(nix): move herdr to shell packages

--- nix/home/packages/editor.nix
+++ nix/home/packages/editor.nix
@@ -2,7 +2,6 @@
 {
   home.packages = with pkgs; [
     claude-code
-    herdr
     luarocks
     neovim
     tree-sitter
--- nix/home/packages/shell.nix
+++ nix/home/packages/shell.nix
@@ -8,6 +8,7 @@
     eza
     fd
     fzf
+    herdr
     hyperfine
     jq
     lolcat
