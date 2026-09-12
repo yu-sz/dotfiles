13bcb658296becf17cac0ce8a01bcbec91d00f33
yu-sz <yusuzuf00@gmail.com>
2026-09-12 13:54:43 +0900

    feat(nvim): reset IME to ASCII on TermLeave

--- config/nvim/lua/plugins/im-select.lua
+++ config/nvim/lua/plugins/im-select.lua
@@ -10,9 +10,11 @@ return {
     -- command
     default_command = "macism",
     -- 以下のイベント時に、デフォルトのIMEになる
+    -- TermLeave: terminal-mode では InsertLeave が発火しないため、
+    -- sidekick の CLI 等から抜けた際はこれで英数へ戻す
     -- FocusGained は入れない。日本語変換中に入力ソースを奪って未確定文字列を壊すうえ、
     -- InsertLeave 直後に発火すると復帰用の保存値が ABC で上書きされ、英数しか入らなくなる
-    set_default_events = { "VimEnter", "InsertLeave", "CmdlineLeave" },
+    set_default_events = { "VimEnter", "InsertLeave", "CmdlineLeave", "TermLeave" },
     -- 以下のイベント時に、前回使われていたIMEになる
     set_previous_events = { "InsertEnter" },
   },
