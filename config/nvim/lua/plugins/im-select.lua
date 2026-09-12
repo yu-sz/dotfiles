f1e50059959f905130ef335d51475732a22789f2
yu-sz <yusuzuf00@gmail.com>
2026-09-12 13:54:43 +0900

    fix(nvim): drop FocusGained from im-select default events

--- config/nvim/lua/plugins/im-select.lua
+++ config/nvim/lua/plugins/im-select.lua
@@ -10,8 +10,9 @@ return {
     -- command
     default_command = "macism",
     -- 以下のイベント時に、デフォルトのIMEになる
-    -- FocusGained: 他アプリ/ペインで日本語入力したままノーマルモードに戻るケースを補足
-    set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
+    -- FocusGained は入れない。日本語変換中に入力ソースを奪って未確定文字列を壊すうえ、
+    -- InsertLeave 直後に発火すると復帰用の保存値が ABC で上書きされ、英数しか入らなくなる
+    set_default_events = { "VimEnter", "InsertLeave", "CmdlineLeave" },
     -- 以下のイベント時に、前回使われていたIMEになる
     set_previous_events = { "InsertEnter" },
   },
